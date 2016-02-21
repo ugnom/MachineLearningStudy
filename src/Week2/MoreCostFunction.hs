05d7ee29d6d2a71aba4a8cd1472533f83cd7904d
ugnom <ugnom1121@gmail.com>
2016-02-21 08:30:10 -0500

    a

--- src/Week2/MoreCostFunction.hs
+++ src/Week2/MoreCostFunction.hs
@@ -10,21 +10,17 @@ type InputSet = Vector Double
 data TrainingSet = TrainingSet { x :: InputSet, y :: Double }
   deriving (Show)
 
+--Hypotethis of the learning, applying parameter after leaned
 hyp :: Parameter -> InputSet -> Double
 hyp theta x = (multStd (transpose (colVector theta)) (colVector x)) ! (1,1)
 
+--error function, how much error rate it gets with a given parameter
 errorj :: Parameter -> [TrainingSet] -> Double
 errorj theta tsets = 
   let h = hyp theta
   in (1 / (2 * (fromIntegral (length tsets)))) * (sum (map (\t -> ((h (x t)) - (y t))^2) tsets))
 
-descentOne :: Double -> [TrainingSet] -> Parameter -> Parameter
-descentOne alpha ts ps = fromList $ map (updateTheta) $ toList ps
-  where 
-    h = hyp ps
-    m = fromIntegral . length $ ts
-    updateTheta theta = theta - alpha * (sum (map (\t -> ((h (x t)) - (y t))) ts) / m)
-
+--Gradient Decent with multi-parameter/input values. 
 gradientDescent :: Double -> Double -> [TrainingSet] -> Parameter -> Parameter
 gradientDescent threshold alpha ts p = inGD p
   where
@@ -32,6 +28,21 @@ gradientDescent threshold alpha ts p = inGD p
       let up = d1 p  
       in if threshold > abs ((errorj p ts) - (errorj up ts))then p else inGD up 
     d1 = descentOne alpha ts
+
+descentOne :: Double -> [TrainingSet] -> Parameter -> Parameter
+descentOne alpha ts ps = fromList $ map (updateTheta) $  zip [0..] (toList ps)
+  where 
+    h = hyp ps
+    m = fromIntegral . length $ ts
+    updateTheta (n, theta) = 
+      theta - alpha * (sum (map (\t -> ((h (x t)) - (y t)) * ((toList (x t)) !! n)) ts) / m)
+
+
+--scale a input feature to roughly -1 to 1 (which makes gradient descent faster)
+--the way the feature has been scaled should be kept some where to apply it later to test set?
+-- (value - ave) / (max - min) <-- we can apply S.D. concept but don't have to 
+scaleFeature :: [Double] -> [Double]
+scaleFeature = undefined
 
 {-
 --creating sample data set by y = x with some randomness
